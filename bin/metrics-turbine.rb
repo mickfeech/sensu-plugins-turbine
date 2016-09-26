41b5b07b5662face5207e52e5b9f25fe9678b4f4
Chris McFee <christopher_mcfee@keybank.com>
2016-09-26 14:19:33 -0400

    initial commit

---
+++ bin/metrics-turbine.rb
@@ -0,0 +1,76 @@
+#! /usr/bin/env ruby
+#
+#   metrics-turbine
+#
+# DESCRIPTION:
+#   Will output data from turbine.stream
+#
+# OUTPUT:
+#   metric data
+#
+# PLATFORMS:
+#   Linux
+#
+# DEPENDENCIES:
+#   gem: sensu-plugin
+#   gem: eventmachine
+#   gem: em-http-request
+#   gem: json
+#
+# USAGE:
+#
+# NOTES:
+#
+# LICENSE:
+#   Copyright 2016 Chris McFee <cmcfee@kent.edu>
+#   Released under the same terms as Sensu (the MIT license); see LICENSE
+#   for details.
+
+require 'sensu-plugin/metric/cli'
+require 'eventmachine'
+require 'em-http'
+require 'json'
+
+class PodsMetrics < Sensu::Plugin::Metric::CLI::Graphite
+  option :url,
+         short: '-u URL',
+         description: 'Full URL path to the turbine stream',
+         required: true
+
+  option :thread_pool,
+         short: '-t POOLNAME',
+         description: 'The pool name requested',
+         required: true
+
+  def initialize
+    super
+    @url = config[:url]
+    @thread_pool = config[:thread_pool]
+  end
+
+  def run
+    config[:scheme] = URI(@url).host
+    http = EM::HttpRequest.new(@url, keepalive: true, connect_timeout: 0, inactivity_timeout: 0)
+    info = %w(currentActiveCount currentConcurrentExecutionCount)
+    EventMachine.run do
+      req = http.get(head: { 'accept' => 'application/json' })
+      req.stream do |chunk|
+        unless chunk.strip.empty?
+          if chunk.include?('data: ') && !chunk.include?('ping')
+            json_data = (chunk.split(': ')[1]).strip!
+            unless json_data.nil?
+              if json_data.end_with?('}')
+                message = JSON.parse(json_data)
+                if message['name'] == @thread_pool
+                  info.size.times { |i| puts "#{config[:scheme]}.#{message['name']}.#{info[i]}.#{message[info[i]]}" unless message[info[i]].nil? }
+                  EventMachine.stop
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+    ok
+  end
+end
---
+++ sensu-plugins-turbine.gemspec
@@ -0,0 +1,40 @@
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+
+require 'date'
+
+if RUBY_VERSION < '2.0.0'
+  require 'sensu-plugins-turbine'
+else
+  require_relative 'lib/sensu-plugins-turbine'
+end
+
+Gem::Specification.new do |s|
+  s.name          = 'sensu-plugins-turbine'
+  s.version       = SensuPluginsTurbine::Version::VER_STRING
+  s.platform      = Gem::Platform::RUBY
+  s.authors       = ['mickfeech and contributors']
+  s.date          = Date.today.to_s
+  s.email         = ['cmcfee@kent.edu']
+  s.executables   = Dir.glob('bin/**/*.rb').map { |file| File.basename(file) }
+  s.homepage      = 'https://github.com/mickfeech/sensu-plugins-turbine'
+  s.summary       = 'This provides functionality to check turbine streams.'
+  s.description   = 'Plugins to provide functionality to check systemd services for Sensu, a monitoring framework'
+  s.license       = 'MIT'
+  s.has_rdoc      = false
+  s.require_paths = ['lib']
+  s.files         = Dir['lib/**/*.rb']
+  s.test_files    = s.files.grep(%r{^(test|spec|features)/})
+  s.add_runtime_dependency 'sensu-plugin', '~> 1.2'
+  s.add_runtime_dependency 'eventmachine'
+  s.add_runtime_dependency 'em-http-request'
+  s.add_development_dependency 'bundler',                   '~> 1.7'
+  s.add_development_dependency 'codeclimate-test-reporter', '~> 0.4'
+  s.add_development_dependency 'github-markup',             '~> 1.3'
+  s.add_development_dependency 'pry',                       '~> 0.10'
+  s.add_development_dependency 'rake',                      '~> 10.5'
+  s.add_development_dependency 'redcarpet',                 '~> 3.2'
+  s.add_development_dependency 'rubocop',                   '~> 0.37'
+  s.add_development_dependency 'rspec',                     '~> 3.4'
+  s.add_development_dependency 'yard',                      '~> 0.8'
+end
